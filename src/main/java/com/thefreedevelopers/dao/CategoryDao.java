f8d19d43e1af2457c739cf3bb5910a3ae86ba1e3
Edgar Eldy <ed.eldy21@live.com>
2022-02-26 22:02:11 +0200

    Add saveCategory method

--- src/main/java/com/thefreedevelopers/dao/CategoryDao.java
+++ src/main/java/com/thefreedevelopers/dao/CategoryDao.java
@@ -1,8 +1,34 @@
 package com.thefreedevelopers.dao;
 
+import com.thefreedevelopers.entity.Category;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 /**
  * @author EDGARELDY
  * @Description : Category crud implementation
  */
 public class CategoryDao {
+
+    // Save a category
+    public void saveCategory(String categoryName){
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("default");
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            entityManager.getTransaction().begin();
+            Category category = new Category();
+            category.setCategoryName(categoryName);
+            entityManager.persist(category);
+            entityManager.getTransaction().commit();
+        } catch (Exception ex) {
+            if (entityManager != null) {
+                entityManager.getTransaction().rollback();
+            }
+            ex.printStackTrace();
+        } finally {
+            entityManagerFactory.close();
+        }
+    }
 }
