046b0293c3e5bce31c790ef52edeb3ca1143f3a9
Edgar Eldy <ed.eldy21@live.com>
2022-02-26 22:02:25 +0200

    Call saveCategory in the App class

--- src/main/java/com/thefreedevelopers/main/App.java
+++ src/main/java/com/thefreedevelopers/main/App.java
@@ -1,7 +1,10 @@
 package com.thefreedevelopers.main;
 
+import com.thefreedevelopers.dao.CategoryDao;
+
 public class App {
     public static void main(String[] args) {
-        
+        CategoryDao categoryDao = new CategoryDao();
+        categoryDao.saveCategory("Lemonades");
     }
 }
