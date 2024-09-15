445c84b03620b4aa1c556a5f8d6519aae2f5ae49
EdgarEldy <ed.eldy21@live.com>
2024-09-15 14:32:04 +0200

    Add category_id foreign key and generate getter and setter

--- src/main/java/com/thefreedevelopers/entity/Product.java
+++ src/main/java/com/thefreedevelopers/entity/Product.java
@@ -10,11 +10,23 @@ public class Product {
     @Column(name = "id", nullable = false)
     private Long id;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+
     public Long getId() {
         return id;
     }
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
     }
 }
