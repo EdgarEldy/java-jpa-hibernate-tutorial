61823ee2bbe519b34c43f0fb77a54da0f4b15362
EdgarEldy <ed.eldy21@live.com>
2024-09-15 13:09:44 +0200

    Add Id column and generate getter and setter

--- src/main/java/com/thefreedevelopers/entity/Product.java
+++ src/main/java/com/thefreedevelopers/entity/Product.java
@@ -1,9 +1,20 @@
 package com.thefreedevelopers.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "products")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
